a64bf49fc1ee9f46a28f9c85c68a8d6f7cb0fcc0
daniel <tohaodan@163.com>
2015-07-24 15:02:27 +0800

    alert

--- src/main/java/com/zc/web/action/player/SetAlertAction.java
+++ src/main/java/com/zc/web/action/player/SetAlertAction.java
@@ -1,9 +1,16 @@
 package com.zc.web.action.player;
 
+import org.apache.commons.beanutils.PropertyUtils;
+
 import com.zc.web.action.PBBaseAction;
 import com.zc.web.core.PBRequestSession;
+import com.zc.web.data.model.Player;
+import com.zc.web.data.model.Player.Alert;
+import com.zc.web.exception.SmallException;
+import com.zc.web.message.ErrorCodeProto.ErrorCode;
 import com.zc.web.message.PBMessage;
 import com.zc.web.message.player.AlertMsgProto.AlertMsg;
+import com.zc.web.service.PlayerService;
 
 public class SetAlertAction extends PBBaseAction {
 
@@ -11,11 +18,17 @@ public class SetAlertAction extends PBBaseAction {
 	public void done(PBRequestSession reqSession, PBMessage request,
 			PBMessage response) throws Exception {
 		
-		
+		Player player = reqSession.getPlayer();
+		if(player.getVip() <= 0){
+			throw new SmallException(ErrorCode.ERR_AUTHORIZED_FAILED);
+		}
 		
 		AlertMsg req = (AlertMsg)getReq(request, AlertMsg.newBuilder());
+		if(player.getAlert() == null)
+			player.setAlert(new Alert());
+		PropertyUtils.copyProperties(player.getAlert(), req);
 		
-		
+		PlayerService.savePlayer(player);
 	}
 
 }
