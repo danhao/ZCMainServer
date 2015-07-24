704a22c32f488dc1d6898d7cf252cbd5ebb1357c
daniel <tohaodan@163.com>
2015-07-24 15:32:25 +0800

    test

--- src/main/java/com/zc/web/action/debt/CreateDebtAction.java
+++ src/main/java/com/zc/web/action/debt/CreateDebtAction.java
@@ -28,7 +28,7 @@ public class CreateDebtAction extends PBBaseAction {
 		
 		String from = reqSession.getChannel().remoteAddress().toString();
 		
-		Debt debt = DebtService.createDebt(builder.build(), reqSession.getPlayer(), from.indexOf("218.17.157.170") >= 0);
+		Debt debt = DebtService.createDebt(builder.build(), reqSession.getPlayer(), reqSession.getPlayer().getId()==624419524266954752L);
 		if(debt != null)
 			response.setRsp(debt.build());
 	}
