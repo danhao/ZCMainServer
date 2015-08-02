3b2c445b76222e63ccb72b97fcddde9dcbcbb44d
danhao <tohaodan@163.com>
2015-08-02 20:47:10 +0800

    count

---
+++ src/main/java/com/zc/web/action/player/DebtCountCmd.java
@@ -0,0 +1,34 @@
+package com.zc.web.action.player;
+
+import java.util.Map.Entry;
+
+import com.zc.web.action.PBBaseAction;
+import com.zc.web.config.ConfigHelper;
+import com.zc.web.config.model.ConfigVip;
+import com.zc.web.core.PBRequestSession;
+import com.zc.web.data.model.Player;
+import com.zc.web.message.PBMessage;
+import com.zc.web.message.common.SingleMsgProto.SingleMsg;
+
+public class DebtCountCmd extends PBBaseAction {
+
+	@Override
+	public void done(PBRequestSession reqSession, PBMessage request,
+			PBMessage response) throws Exception {
+		Player player = reqSession.getPlayer();
+		
+		ConfigVip vip = ConfigHelper.getConfigVip(player.getVip());
+		int limit = (vip == null ? 100 : vip.getBidLimit());
+		int count = 0;
+		for(Entry<Long, Boolean> entry : player.getBidDebts().entrySet()){
+			if(!entry.getValue())
+				count ++;
+		}
+		
+		SingleMsg.Builder rsp = SingleMsg.newBuilder();
+		rsp.setParam(String.valueOf(limit - count));
+		
+		response.setRsp(rsp.build());
+	}
+
+}
