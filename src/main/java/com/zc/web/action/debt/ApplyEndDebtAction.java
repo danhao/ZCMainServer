612dfe65b45a51f684c827fb6aa01dd66686c6ce
danhao <tohaodan@163.com>
2015-08-08 18:33:22 +0800

    end

---
+++ src/main/java/com/zc/web/action/debt/ApplyEndDebtAction.java
@@ -0,0 +1,43 @@
+package com.zc.web.action.debt;
+
+import com.zc.web.action.PBBaseAction;
+import com.zc.web.core.Constant;
+import com.zc.web.core.PBRequestSession;
+import com.zc.web.data.model.Debt;
+import com.zc.web.data.model.Player;
+import com.zc.web.exception.SmallException;
+import com.zc.web.message.ErrorCodeProto.ErrorCode;
+import com.zc.web.message.PBMessage;
+import com.zc.web.message.common.SingleMsgProto.SingleMsg;
+import com.zc.web.service.DebtService;
+import com.zc.web.service.PlayerService;
+
+public class ApplyEndDebtAction extends PBBaseAction {
+
+	@Override
+	public void done(PBRequestSession reqSession, PBMessage request,
+			PBMessage response) throws Exception {
+		SingleMsg req = (SingleMsg)getReq(request, SingleMsg.newBuilder());
+		
+		Player player = reqSession.getPlayer();
+		long debtId = Long.parseLong(req.getParam());
+		
+		Debt debt = DebtService.getDebtById(debtId);
+		if(debt == null || debt.getState() != Constant.STATE_DEALED)
+			throw new SmallException(ErrorCode.ERR_DEBT_INVALID);
+
+		if(debt.getWinnerId() != player.getId()){
+			throw new SmallException(ErrorCode.ERR_DEBT_INVALID);
+		}
+		
+		if(debt.getRepayments().size() == 0)
+			throw new SmallException(ErrorCode.ERR_DEBT_INVALID);
+		
+		if(player.getApplyEndDebts().contains(debtId))
+			return;
+		
+		player.getApplyEndDebts().add(debtId);
+		PlayerService.savePlayer(player);
+	}
+
+}
